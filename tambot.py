9798a8bf5e0acfd1323b16b84a297e43bd3f7ea3
Tamboru <kayo.henrique1176@gmail.com>
2020-06-15 21:15:57 -0300

    Upado para o Git

---
+++ tambot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.6
+
+# Variáveis e listas:
+info = "uashdua"
+
+# Script
+
+print ("\033[1;34m--------------------------------------------------------------\033[1;34m")
+print ("|                                                            |")
+print ("|                   Bem vindo ao chatbot                     |")
+print ("|                                                            |")
+print ("--------------------------------------------------------------")
+print ()
+print ("\033[1;35mBot" "\033[0m: Olá!")
+
+
+
+while(info != "Tchau"):
+
+	info = input("\033[1;32mVocê" "\033[0m: ")
+
+	if info == ("Tchau"):
+		
+		print ("\033[1;35mBot" "\033[0m: Ok, até a próxima!")
+		exit()
+		
+	else:
+
+		try:
+			responder = open(".cerebro/" + info, "r")
+	
+		except IOError:
+			print ("\033[1;35mBot" "\033[0m: Ainda não sei o que você quis dizer, o que eu deveria responder?")
+			resposta = input ("\033[1;32mVocê" "\033[0m: ")
+			novo_arquivo = open(".cerebro/" + info, "a") 
+			novo_arquivo.write(resposta)
+			print ("\033[1;35mBot" "\033[0m: Certo, devo falar " "\033[0;34m[", resposta, "]")
+			
+		else:
+			responder = open(".cerebro/" + info, "r")
+			falar = responder.read()
+			print ("\033[1;35mBot" "\033[0m: " + falar)
