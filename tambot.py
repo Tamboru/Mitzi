fed8c8ad8422585b83a894d9c86775cdf5a59969
Tamboru <kayo.henrique1176@gmail.com>
2020-06-16 08:26:23 -0300

    Melhorias e adição de um nome

--- tambot.py
+++ tambot.py
@@ -7,14 +7,14 @@ info = "uashdua"
 
 print ("\033[1;34m--------------------------------------------------------------\033[1;34m")
 print ("|                                                            |")
-print ("|                   Bem vindo ao chatbot                     |")
+print ("|                Bem vindo ao chatbot Mitzi                  |")
 print ("|                                                            |")
 print ("--------------------------------------------------------------")
 print ()
 
-print ("\033[1;35mBot" "\033[0m: Olá! Qual é seu nome?")
+print ("\033[1;35mMitzi" "\033[0m: Olá! Qual é seu nome?")
 nome = input("\033[1;32mVocê" "\033[0m: ")
-print ("\033[1;35mBot" "\033[0m: Olá", nome+"," " como vai você?")
+print ("\033[1;35mMitzi" "\033[0m: Olá", nome+"," " como vai você?")
 
 while(info != "tchau"):
 
@@ -22,7 +22,7 @@ while(info != "tchau"):
 	info = info.lower()
 	if info == ("tchau"):
 		
-		print ("\033[1;35mBot" "\033[0m: Ok, até a próxima!")
+		print ("\033[1;35mMitzi" "\033[0m: Ok, até a próxima!")
 		exit()
 		
 	else:
@@ -31,13 +31,13 @@ while(info != "tchau"):
 			responder = open(".cerebro/" + info, "r")
 	
 		except IOError:
-			print ("\033[1;35mBot" "\033[0m: Ainda não sei o que você quis dizer, o que eu deveria responder?")
+			print ("\033[1;35mMitzi" "\033[0m: Ainda não sei o que você quis dizer, o que eu deveria responder?")
 			resposta = input ("\033[1;32mVocê" "\033[0m: ")
 			novo_arquivo = open(".cerebro/" + info, "a") 
 			novo_arquivo.write(resposta)
-			print ("\033[1;35mBot" "\033[0m: Certo, devo falar " "\033[0;34m[", resposta, "]")
+			print ("\033[1;35mMitzi" "\033[0m: Certo, devo falar " "\033[0;34m[", resposta, "]")
 			
 		else:
 			responder = open(".cerebro/" + info, "r")
 			falar = responder.read()
-			print ("\033[1;35mBot" "\033[0m: " + falar)
+			print ("\033[1;35mMitzi" "\033[0m: " + falar)
